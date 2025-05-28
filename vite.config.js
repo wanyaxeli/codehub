e92095831c8c08cdddb3ca0657d87fd8881c7b46
wanyaxeli <eliwanyax@gmail.com>
2025-05-28 18:33:59 +0300

    404 page

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import fs from 'fs';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   base: '/codehub/',
   plugins: [
     react(),
     nodePolyfills(),
-    {
-      // 👇 This plugin copies 404.html to dist/ after build
-      name: 'copy-404',
-      closeBundle() {
-        try {
-          fs.copyFileSync('public/404.html', 'dist/404.html');
-          console.log('404.html copied to dist/');
-        } catch (err) {
-          console.error('Failed to copy 404.html:', err);
-        }
-      },
-    },
   ],
   define: {
     global: 'window', // Define global for Simple-Peer
   },
   resolve: {
     alias: {
-      buffer: 'buffer',
+      buffer: 'buffer', // Ensure buffer is available
     },
   },
 });
