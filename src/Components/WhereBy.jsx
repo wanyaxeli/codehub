d7cfd4b19adfa80c75b2d80781c83e59c354c4f3
wanyaxeli <eliwanyax@gmail.com>
2025-07-21 10:27:42 +0300

    register update

--- src/Components/WhereBy.jsx
+++ src/Components/WhereBy.jsx
@@ -1,8 +1,12 @@
 export default function WherebyIframe({ roomUrl }) {
+   // Append `showLogo=false` if it's not already in the URL
+   const formattedUrl = roomUrl.includes('?')
+   ? `${roomUrl}&showLogo=false`
+   : `${roomUrl}?showLogo=false`;
   return (
     <div>
       <iframe
-        src={roomUrl}
+        src={formattedUrl}
         allow="camera; microphone; fullscreen; speaker; display-capture"
         style={{ width: '100%', height: '80vh', border: '0', borderRadius: '12px' }}
         title="Codingschalor Meeting"
